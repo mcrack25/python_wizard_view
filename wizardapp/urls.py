f0611f46a52b4c3647e76e67efb9a8e4b07eac4a
mcrack25 <mcrack@yandex.ru>
2021-09-17 17:39:21 +0300

    Добавил второй способ добавления, с сохранением в конце

--- wizardapp/urls.py
+++ wizardapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import WelcomeWizard_as
+from .views import WelcomeWizard_as, WelcomeWizard_ls
 
 urlpatterns = [
+    path('', WelcomeWizard_as.as_view(WelcomeWizard_as._form_list, condition_dict=WelcomeWizard_as._condition_dict), name='welcomeform_autosave_first',),
     path('autosave/', WelcomeWizard_as.as_view(WelcomeWizard_as._form_list, condition_dict=WelcomeWizard_as._condition_dict), name='welcomeform_autosave',),
+    path('latesave/', WelcomeWizard_ls.as_view(WelcomeWizard_ls._form_list, condition_dict=WelcomeWizard_ls._condition_dict), name='welcomeform_latesave',),
 ]
--- wizardapp/views.py
+++ wizardapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Person, Messages
+from .models import Person
 from .forms import FLNameForm, SNameForm, MessagesInlineFormSet
 from formtools.wizard.views import SessionWizardView
 
@@ -7,6 +7,7 @@ STEP_ONE = '0'
 STEP_TWO = '1'
 STEP_THREE = '2'
 
+# Автоматическое сохранение данных на каждом шаге
 class WelcomeWizard_as(SessionWizardView):
     template_name = 'wizardapp/form.html'
     person = Person()
@@ -74,3 +75,82 @@ class WelcomeWizard_as(SessionWizardView):
         (STEP_THREE, SNameForm),
     ]
 
+
+# Сохранение данных в конце
+class WelcomeWizard_ls(SessionWizardView):
+    template_name = 'wizardapp/form.html'
+
+    def gen_request(self, data, step):
+        count_forms = str(len(data))
+        del_items = ['DELETE', 'person']
+
+        result = {
+            '{0}-TOTAL_FORMS'.format(step): count_forms,
+            '{0}-INITIAL_FORMS'.format(step): '0',
+            '{0}-MIN_NUM_FORMS'.format(step): '0',
+            '{0}-MAX_NUM_FORMS'.format(step): '1000',
+        }
+        i = 0
+        for item in data:
+            new_item = {}
+            for el in item:
+                if not (el in del_items):
+                    key = '{0}-{1}-{2}'.format(step, i, el)
+                    if item[el] == None:
+                        val = ''
+                    else:
+                        val = item[el]
+                    new_item.update({key: val})
+            result.update(new_item)
+            i += 1
+        return result
+
+    def done(self, form_list, **kwargs):
+        step_1 = self.get_cleaned_data_for_step(STEP_ONE)
+        step_2 = self.get_cleaned_data_for_step(STEP_TWO)
+        step_3 = self.get_cleaned_data_for_step(STEP_THREE)
+
+        person = Person()
+        person.fname = step_1['fname']
+        person.lname = step_1['lname']
+        if step_3:
+            person.sname = step_3['sname']
+        person.save()
+
+        req = self.gen_request(step_2, STEP_TWO)
+        formset = MessagesInlineFormSet(req, self.request.FILES, instance=person, prefix=STEP_TWO)
+        for form in formset:
+            if form.is_valid():
+                form.save(commit=False)
+        if formset.is_valid():
+            formset.save()
+
+        return render(self.request, 'wizardapp/done.html', {
+            'form_data': [form.cleaned_data for form in form_list],
+        })
+
+    # Custom methods
+    def return_true(self):
+        return True
+
+    def check_step_3(self):
+        result = True
+        step_info = self.get_cleaned_data_for_step(STEP_ONE)
+        try:
+            if (step_info['no_sname']):
+                result = False
+        except:
+            pass
+        return result
+
+    _condition_dict = {
+        STEP_ONE: return_true,
+        STEP_TWO: return_true,
+        STEP_THREE: check_step_3,
+    }
+
+    _form_list = [
+        (STEP_ONE, FLNameForm),
+        (STEP_TWO, MessagesInlineFormSet),
+        (STEP_THREE, SNameForm),
+    ]
